0d609da76bd152a3d7d50be2b094666258ea8f92
romainsim <rom.simon32@gmail.com>
2018-02-15 19:04:20 +0100

    Add files via upload

--- Niveau.java
+++ Niveau.java
@@ -136,25 +136,49 @@ public class Niveau extends JPanel{
     public int getVie() {
 	return this.nbVie;
     }
-    
+
+    public void setVie(int v) {
+	this.nbVie = v;
+    }
+	
     public int getCadence() {
         return this.nbCadence;
+    }
+
+    public void setCadence(int c) {
+	this.nbCadence = c;
     }
     
     public int getBalle() {
     	     return this.nbBalle;
     }
+
+    public void setBalle(int b) {
+	this.nbBalle = b;
+    }
     
     public int getDispersion() {
     	     return this.nbDispersion;
+    }
+
+    public void setDispersion(int d)  {
+	this.nbDispersion = d;
     }
     
     public int getEnnemis() {
 	     return this.nbEnnemi;
     }
+
+    public void setEnnemis(int e) {
+	this.nbEnnemi = e;
+    }
     
     public int getLevel() {
     	return this.level;
+    }
+
+    public void setLevel(int l) {
+	this.level = l;
     }
     
     class BoutonListener implements ActionListener{
--- Sauvegarde.java
+++ Sauvegarde.java
@@ -1,12 +1,57 @@
 import java.awt.*;
-import java.awt.Event.*;
+import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
-import java.io.*;
+import java.sql.*;
 
-class Sauvegarde {
+class Sauvegarde implements ActionListener {
 
-   
+    public Niveau n;
+    public MenuController mc;
+    public String pseudo;
+    public int vie;
+    public int cadence;
+    public int dispersion;
+    public int enemis;
+    public int level;
+    public int munitions;
+    
+    public Sauvegarde () {
+	this.pseudo = mc.nom;
+	this.vie = n.getVie();
+	this.cadence = n.getCadence();
+	this.dispersion = n.getDispersion();
+	this.enemis = n.getEnnemis();
+	this.level = n.getLevel();
+	this.munitions = n.getBalle();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	try {
+	    Class.forName("org.mariadb.jdbc.Driver");
+	}
+	catch (ClassNotFoundException b) {
+	    System.err.println("Pilote indisponible!");
+	}
+
+	try {
+	    Connection connexion = DriverManager.getConnection("jdbc:mardiadb://dwarves.iut-fbleau.fr/simonr","simonr","Azertyuiop");
+
+	    PreparedStatement insertVal = connexion.prepareStatement("INSERT INTO Sauvegarde VALUES('"+pseudo+"',"+vie+","+cadence+","+munitions+","+dispersion+","+enemis+","+level);
+	    ResultSet sauvegardeComplete = insertVal.executeQuery();
+	    if (sauvegardeComplete.first()) {
+		JOptionPane jop = new JOptionPane();
+		jop.showMessageDialog(null,"Sauvegarde réussie!","Information",JOptionPane.INFORMATION_MESSAGE);
+	    }
+	    insertVal.close();
+	    sauvegardeComplete.close();
+	    connexion.close();
+	} catch (SQLException b) {
+	    System.err.println("Erreur de connexion: "+ b.getMessage());
+
+	}
+    }
 }
 
 	
