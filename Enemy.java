f8837d225e36d581929f15cac93bd702f26b91e9
florianvazelle <ponythugflorian@gmail.com>
2018-02-24 15:00:12 +0100

    soft optimisation

--- Enemy.java
+++ Enemy.java
@@ -9,6 +9,7 @@ public class Enemy extends Personnage{
     ControlerEnemy ce;
     ArrayList<Node> path;
     AStar algo;
+    int[][] ancienPos;
     
     public Enemy(String nom, int vie, String skin, int x_de_base, int y_de_base, boolean[][] map, Terrain t){
 	super(nom, vie, skin, x_de_base, y_de_base);
@@ -18,6 +19,12 @@ public class Enemy extends Personnage{
 	this.speed = 2;
 	this.t = t;
 	this.algo = new AStar(76, 41, map, this, t);
+
+	this.ancienPos = new int[2][2];
+	this.ancienPos[0][0] = 0;
+	this.ancienPos[0][1] = 0;
+	this.ancienPos[1][0] = 0;
+	this.ancienPos[1][1] = 0;
     }
     
     void setCe(ControlerEnemy ce){
@@ -48,21 +55,31 @@ public class Enemy extends Personnage{
 	    awake = true;
 	    
 	}
+
+	int varX = super.getCoordonneX();
+	int varY = super.getCoordonneY();
 	
 	/*if(awake && distance(super.getCoordonneX(), super.getCoordonneY(), playerPosX, playerPosY) < 400)
 	  ce.tirer();
 	*/
 	if(awake){
-	    if(distance(super.getCoordonneX(), super.getCoordonneY(), playerPosX, playerPosY) > 100 )
+	    System.out.println("ancienPos[0][0] "+ancienPos[0][0]+" | ancienPos[0][1]"+ancienPos[0][1]+" | ancienPos[1][0]"+ancienPos[1][0]+" | ancienPos[1][1]"+ancienPos[1][1]);
+	    if(distance(varX, varY, playerPosX, playerPosY) > 100 && !(ancienPos[0][0] == varX/25 && ancienPos[0][1] == varY/25 && ancienPos[1][0] == playerPosX/25 && ancienPos[1][1] == playerPosY/25) ){
+		System.out.println("here");
 		path = algo.findPath(new Node(super.getCoordonneX()/25, super.getCoordonneY()/25), new Node(playerPosX/25, playerPosY/25));
+		ancienPos[0][0] = varX/25;
+		ancienPos[0][1] = varY/25;
+		ancienPos[1][0] = playerPosX/25;
+		ancienPos[1][1] = playerPosY/25;
+	    }
 	    
 	    if(!path.isEmpty()){
 		
 		int targetX = path.get(path.size() - 1).getX();
 		int targetY = path.get(path.size() - 1).getY();
 	
-		super.setCoordonneX(approachValues(super.getCoordonneX(), targetX*25, 3));
-		super.setCoordonneY(approachValues(super.getCoordonneY(), targetY*25, 3));
+		super.setCoordonneX(approachValues(varX, targetX*25, 3));
+		super.setCoordonneY(approachValues(varY, targetY*25, 3));
 	    }
 	}
     }
