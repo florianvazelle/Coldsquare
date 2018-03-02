54b484e7762a020da69656a8be04d9550677c160
florianvazelle <ponythugflorian@gmail.com>
2018-03-02 20:58:56 +0100

    Update Arme.java

--- Arme.java
+++ Arme.java
@@ -23,20 +23,3 @@ public class Arme {
 	this.cadence = newCadence;
     }
 }
-
- 	    // coefficient directeur
- 	    double m = ((double) (souris.getY() - i.getY()) / (double) (souris.getX() - i.getX()));
- 	    
- 	    // ordonnée a l'origine
- 	    int p = (int) i.getY() - (int) ( m * i.getX() );
- 
- 	    if(i.getX()<souris.getX()){	    
- 		while(x < frame.getWidth() && y < frame.getHeight() && x > 0 && y > 0){
- 		    ba.setX(x++);			
- 		    y = ((int) (m*x) )+  p;
- 		    ba.setY(y);
- 		    
- 		    i = new Point(x,y);
-
-			
-			
