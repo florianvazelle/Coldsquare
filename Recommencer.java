8b0c4787daa56d621e3efa603150ef4a13d15efb
romainsim <rom.simon32@gmail.com>
2018-03-03 13:35:31 +0100

    Recommencer

--- Recommencer.java
+++ Recommencer.java
@@ -7,15 +7,18 @@ class Recommencer implements ActionListener {
 
     private Niveau n;
     private Jeu j;
+	private int score;
     
     public Recommencer(Jeu jeu) {
 	this.j = jeu;
-	
+	n = this.j.getNiveau();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-	j.jouer();
+	    score = n.getScore();
+	j.setScore(score);
+	    j.jouer();
     }
 
 }
